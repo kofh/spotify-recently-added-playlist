96801cba4ff3664cac9e1a885748dfe7a9109825
Tim Morris <timthewebguy@gmail.com>
2020-12-23 09:38:30 -0500

    :wrench: Add role and policy attachment

--- iam.tf
+++ iam.tf
@@ -21,3 +21,27 @@ resource "aws_iam_policy" "ga_upload_lambda_source" {
   name   = "ga-sra-upload-lambda-source"
   policy = data.aws_iam_policy.ga_upload_lambda_source_policy.json
 }
+
+data "aws_iam_policy_document" "ga_upload_lambda_source_role_trust_policy" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "sts:AssumeRole",
+      "sts:TagSession"
+    ]
+    principals {
+      type        = "AWS"
+      identifiers = [data.aws_iam_user.github_actions_deploy_user.arn]
+    }
+  }
+}
+
+resource "aws_iam_role" "ga_upload_lambda_source" {
+  name               = "ga-sra-upload-lambda-source"
+  assume_role_policy = data.aws_iam_policy_document.ga_upload_lambda_source_trust_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "ga_upload_lambda_source_attachment" {
+  role       = aws_iam_role.ga_upload_lambda_source.name
+  policy_arn = aws_iam_policy.ga_upload_lambda_source.arn
+}
